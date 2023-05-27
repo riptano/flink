42f427f8cbe941cd9380df55a9d229ae274299cd
Christophe Bornet <cbornet@hotmail.com>
2023-05-27 11:40:37 +0200

    Fix default database in tests (#3)
    * Fix default database in tests
    
    * Fix default database in tests

--- flink-connectors/flink-connector-pulsar/src/test/java/org/apache/flink/connector/pulsar/table/PulsarChangelogTableITCase.java
+++ flink-connectors/flink-connector-pulsar/src/test/java/org/apache/flink/connector/pulsar/table/PulsarChangelogTableITCase.java
@@ -484,7 +484,6 @@ public class PulsarChangelogTableITCase extends PulsarTableTestBase {
                         FLINK_TENANT));
 
         tableEnvironment.useCatalog(PULSAR_CATALOG);
-        tableEnvironment.executeSql(String.format("CREATE DATABASE %s", PULSAR_DB));
         tableEnvironment.useDatabase(PULSAR_DB);
     }
 }
